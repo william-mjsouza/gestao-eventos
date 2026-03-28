5d5286aee6635d61f133303c654685682bd7d1c3
William Souza <william.ws2121@gmail.com>
2026-03-28 11:02:26 -0300

    feat: esqueleto do projeto

---
+++ src/main/java/com/gestaoeventos/SistemaDeGestaoDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoDeEventosApplication.class, args);
+	}
+
+}
