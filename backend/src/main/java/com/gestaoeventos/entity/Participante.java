db36e686fa2611a577a560c889117d384cac1f62
BrunofCarvalhoo <bcf2@cesar.school>
2026-03-31 16:34:25 -0300

    add entidade participante

---
+++ backend/src/main/java/com/gestaoeventos/entity/Participante.java
@@ -0,0 +1,36 @@
+package com.gestaoeventos.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PARTICIPANTE")
+public class Participante {
+
+    @Id
+    @Column(length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private Double saldo = 0.0;
+
+}
