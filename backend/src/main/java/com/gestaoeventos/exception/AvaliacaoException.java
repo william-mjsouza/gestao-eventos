c634f67e859142be4960ed74e42ac2237e6678c1
biagalindoo <biagalindo4@gmail.com>
2026-04-21 11:49:40 -0300

    Criando entity, service e exeptions

---
+++ backend/src/main/java/com/gestaoeventos/exception/AvaliacaoException.java
@@ -0,0 +1,7 @@
+package com.gestaoeventos.exception;
+
+public class AvaliacaoException extends GestaoEventoException {
+    public AvaliacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
