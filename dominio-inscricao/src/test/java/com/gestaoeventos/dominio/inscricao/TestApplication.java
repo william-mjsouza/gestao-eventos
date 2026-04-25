2b4fec20a11afc1f3d3bfc2255b5160cc872ae41
Arthur Leal <arthurlealp21@gmail.com>
2026-04-25 16:38:24 -0300

    chore(test): padroniza exclusao da autoconfiguracao de banco via application.properties

--- dominio-inscricao/src/test/java/com/gestaoeventos/dominio/inscricao/TestApplication.java
+++ dominio-inscricao/src/test/java/com/gestaoeventos/dominio/inscricao/TestApplication.java
@@ -1,13 +1,9 @@
 package com.gestaoeventos.dominio.inscricao;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = "com.gestaoeventos")
 public class TestApplication {
 }
